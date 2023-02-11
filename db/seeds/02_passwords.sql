feaaece4dd8c3614b1aa0e531ca9af6abdce8aeb
Graham Fujisawa <sfrrki@gmail.com>
2023-02-11 14:14:41 -0800

    add dbschema and seeds (reminder to make a dot env)

---
+++ db/seeds/02_passwords.sql
@@ -0,0 +1,5 @@
+-- Widgets table seeds here (Example)
+INSERT INTO passwords (user_id, url, email, password) VALUES ('1', 'www.facebook.com', 'alice@gmail.com', 'alicethebest2');
+INSERT INTO passwords (user_id, url, email, password) VALUES ('2', 'www.facebook.com', 'kira@telus.net', 'wheremydog');
+INSERT INTO passwords (user_id, url, email, password) VALUES ('2', 'www.twitter.com', 'kira@telus.net', 'wheremydog');
+
