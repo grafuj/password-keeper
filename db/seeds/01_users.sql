938a9e5766d1801612bb3f8941dd390b1c8338d6
vagrant <Juelz Lum>
2023-02-14 23:31:47 +0000

    changed users db to test login

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,3 +2,10 @@ INSERT INTO users (email, password)
 VALUES ('spoon@hotmail.com', 'password'),
 ('graham@outlook.com', 'password'),
 ('juelzlum@gmail.com', 'password');
+
+
+INSERT INTO users(name, email, password)
+VALUES('Julian','juelzlum@gmail.com',123);
+
+INSERT INTO users(name, email, password)
+VALUES('Samson','juelzlum@gmail.com',123);
