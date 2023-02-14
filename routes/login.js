70d706aa2616879279e9c44ba0ef55e35bedddb0
vagrant <Juelz Lum>
2023-02-14 00:41:30 +0000

    creat login page and functions

--- routes/login.js
+++ routes/login.js
@@ -1,52 +1,72 @@
 const express = require('express');
-const { Pool, DatabaseError } = require('pg');
-const { route, use } = require('./users');
-const router  = express.Router();
+const router = express.Router();
+const cookieSession = require('cookie-session');
+const bodyParses = require('body-parser');
+const { Pool } = require('pg');
+
+
+const app = express();
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
+
+const pool = new Pool({
+  host:'localhost',
+  user:'vagrant',
+  password:'123',
+  name:'midterm'
+})
+
+pool.connect()
 
 router.get("/", (req, res) => {
   const templateVars = { value: false };
-  res.render("login", templateVars);
+  res.render("login.ejs", templateVars);
 });
-router.get("/login/:id", (req, res) =>{
-req.session.user_id = req.params.id;
-res.redirect('/users.ejs') //password page, which ever would that be
 
-})
+router.get("/login/:id", (req, res) => {
+  req.session.user_id = req.params.id;
+  res.redirect('/users.ejs'); //password page, which ever would that be
+
+});
 const getUserEmail = (email) => {
   return pool.query(`
   SELECT *
   FROM USERS
-  WHERE EMAIL = $1`, [email].then(resp.rows[0]))
-}
+  WHERE EMAIL = $1`, [email].then(resp.rows[0]));
+};
 
 const getUserPassword = (password) => {
   return pool.query(`
   SELECT *
   FROM USERS
-  WHERE PASSWORD =$1`, [password].then(resp.rows[0]))
-}
+  WHERE PASSWORD =$1`, [password].then(resp.rows[0]));
+};
 
 const login = (email, password) => {
-  if(database.getUserEmail(email) && getUserPassword(password)) {
-    return user_id
+  if (database.getUserEmail(email) && getUserPassword(password)) {
+    return user_id;
   }
 
-}
+};
 
-router.post("/login", (req,res) => {
-  const{email, password} = req.body;
-  login(email,password)
-  .then(user => {
-    if(!user) {
-      res.send({error:"error"})
-      return
-    }
-    req.session.userID = user.id;
-    res.send({user:{name:user.name, email:user.email,id:user.id}});
-  })
-.catch(e => res.send(e))
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  login(email, password)
+    .then(user => {
+      if (!user) {
+        res.send({ error: "error" });
+        return;
+      }
+      req.session.userID = user.id;
+      res.send({ user: { name: user.name, email: user.email, id: user.id } });
+    })
+    .catch(e => res.send(e));
 });
 
 
 
 
+module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,17 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const cookieSession =require('cookie-session')
+const bodyParses = require('body-parser');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}))
+
 
 app.set('view engine', 'ejs');
 
@@ -31,6 +39,7 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const login = require('./routes/login')
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -38,6 +47,7 @@ const usersRoutes = require('./routes/users');
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/login',login);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -45,7 +55,7 @@ app.use('/users', usersRoutes);
 // Separate them into separate routes files (see above).
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render("index");
 });
 
 app.listen(PORT, () => {
