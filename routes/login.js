6967b448a5bda57b3d491dfbed58ab0f78820af0
vagrant <Juelz Lum>
2023-02-15 05:20:28 +0000

    fix login routes

--- routes/login.js
+++ routes/login.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const cookieSession = require('cookie-session');
 const bodyParses = require('body-parser');
-const bcrypt = require('bcrypt');
 const { log } = require('console');
 const { getMaxListeners } = require('process');
 const db = require('../db/connection');
+const { reset } = require('nodemon');
 
 const app = express();
 
@@ -14,18 +14,20 @@ app.use(cookieSession({
   keys: ['key1']
 }));
 
-console.log('Hello world');
 
-// router.get("/", (req, res) => {
-//   const templateVars = { value: false };
-//   res.render("login.ejs", templateVars);
-// });
 
-// router.get("/login/:id", (req, res) => {
-//   req.session.user_id = req.params.id;
-//   res.redirect('/users.ejs'); //password page, which ever would that be
+//get /login/
+router.get("/", (req, res) => {
+  const templateVars = { value: false };
+  res.render("login.ejs", templateVars);
+});
 
-// });
+//get /login/login/:id
+router.get("/:id", (req, res) => {
+  req.session.user_id = req.params.id;
+  res.redirect('/users.ejs'); //password page, which ever would that be
+
+});
 
 //'juelzlum@gmail.com', 'password'
 const getUserEmail = (email) => {
@@ -33,38 +35,47 @@ const getUserEmail = (email) => {
   SELECT *
   FROM USERS
   WHERE EMAIL = $1`, [email])
-  .then(resp => console.log(resp.rows));
+  .then(resp => (resp.rows[0]));
 };
 
+getUserEmail('juelzlum@gmail.com')
+
 // const getUserPassword = (password) => {
-//   return pool.query(`
+//   return db.query(`
 //   SELECT *
 //   FROM USERS
-//   WHERE PASSWORD =$1`, [password].then(resp.rows[0]));
+//   WHERE PASSWORD =$1`, [password]).then(resp => console.log(resp.rows))
 // };
+// getUserPassword('password')
+
+
 
 const login = function(email, password) {
   return getUserEmail(email)
   .then(user => {
-    if (bcrypt.compareSync(password, user.password)) {
-      return user;
+    if(password !== user.password) {
+
+      return null
     }
-    return null;
-  });
+    return user
+  })
 }
+// login('juelzlum@gmail.com, password')
+
+// /login/login
 
 router.post("/", (req, res) => {
   const { email, password } = req.body;
   login(email, password)
     .then(user => {
       if (!user) {
-        res.send({ error: "error" });
+        res.send({error: 'user not found!'});
         return;
       }
       req.session.userID = user.id;
-     console.log({ user: { name: user.name, email: user.email, id: user.id } });
+     res.send({ user: { name: user.name, email: user.email, id: user.id } });
     })
-    .catch(err => console.log('error:', err.message));
+    .catch(err => res.send('error:', err.message));
 });
 
 
