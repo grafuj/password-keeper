7dad97a34a5170c8a09296a55f588aa379faeef8
Samson Poon <samsonkfpoon@gmail.com>
2023-02-15 01:30:21 +0000

    minor changes

--- db/schema/03_users.sql
+++ db/schema/03_users.sql
@@ -4,6 +4,5 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  organization_id INTEGER REFERENCES organizations(id) ON DELETE CASCADE
+  password VARCHAR(255) NOT NULL
 );
---
+++ routes/login.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const cookieSession = require('cookie-session');
+const bodyParses = require('body-parser');
+const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
+
+const app = express();
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
+
+const pool = new Pool({
+  host:'localhost',
+  user:'vagrant',
+  password:'123',
+  name:'midterm'
+})
+
+pool.connect()
+
+router.get("/", (req, res) => {
+  const templateVars = { value: false };
+  res.render("login.ejs", templateVars);
+});
+
+router.get("/login/:id", (req, res) => {
+  req.session.user_id = req.params.id;
+  res.redirect('/users.ejs'); //password page, which ever would that be
+
+});
+
+//'juelzlum@gmail.com', '123'
+const getUserEmail = (email) => {
+  return pool.query(`
+  SELECT *
+  FROM USERS
+  WHERE EMAIL = $1`, [email])
+  .then(resp => console.log(resp.rows));
+};
+
+
+// const getUserPassword = (password) => {
+//   return pool.query(`
+//   SELECT *
+//   FROM USERS
+//   WHERE PASSWORD =$1`, [password].then(resp.rows[0]));
+// };
+
+const login = function(email, password) {
+  return getUserEmail(email)
+  .then(user => {
+    if (bcrypt.compareSync(password, user.password)) {
+      return user;
+    }
+    return null;
+  });
+}
+router.post("/", (req, res) => {
+  const { email, password } = req.body;
+  login(email, password)
+    .then(user => {
+      if (!user) {
+        res.send({ error: "error" });
+        return;
+      }
+      req.session.userID = user.id;
+     console.log({ user: { name: user.name, email: user.email, id: user.id } });
+    })
+    .catch(err => console.log(err.message));
+});
+
+
+
+
+module.exports = router;
