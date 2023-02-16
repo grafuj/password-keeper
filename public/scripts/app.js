e9963286c77b6abbc34d5e8f849c75a165ae7b5d
vagrant <Juelz Lum>
2023-02-16 02:46:58 +0000

    add generate function and single page web with ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,69 @@
-const helper = require('../../routes/helper-functions');
+
 
 
 // Client facing scripts here
 $(() => {
-  document.getElementById("pw-generate")
-    .onclick = () => {
-      helper.generateRandomString
+
+  $('#generatebutton').on('click', function() {
+    const myLength = $('#pw-length').val()
+    const lowerCase = $('#pw-lowercase').is(':checked')
+    const upperCase =$('#pw-uppercase').is(':checked')
+    const number = $('#pw-number').is(':checked')
+    const sym = $('#pw-symbol').is(':checked')
+
+    console.log('variables:', myLength, lowerCase, upperCase, number, sym)
+    const password = genPassword(myLength, lowerCase, upperCase, number, sym)
+
+    //put the password into the pw entry field
+    $("#pw-entry").val(password)
+  })
+
+  //returns a random string of desired length, with booleans to determine what the password should contain
+  const genPassword = (length = 13, containLowercase = true, containUppercase = false, containNumbers = true, containSymbols = false) => {
+
+    let bank = "";
+    if (containLowercase) {
+      let lowercase = 'abcdefghijklmnoqrstuvwxyz';
+      bank += lowercase;
     }
-})
+    if (containUppercase) {
+      let uppercase = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+      bank += uppercase;
+    }
+    if (containNumbers) {
+      let numbers = "0123456789";
+      bank += numbers;
+    }
+    if (containSymbols) {
+      let symbols = "!@#$%^&*()_+-=,.<>";
+      bank += symbols;
+    }
+
+    let randStr = "";
+    for (let i = 0; i < length; i++) {
+      randStr += bank[Math.floor(bank.length * Math.random())];
+    }
+    return randStr;
+  };
+  //can't use arrow functions,we want to use .this which will refer to what called the function as opposed to the parent
+  $('form').on('submit', function(event) {
+    event.preventDefault()
+    const data = $(this).serialize()
+    $.post('/api/passwords', data)
+      .then(function(result){
+        const element =  $(`<h3>
+        ${result.username}
+        ${result.url}
+        ${result.email}
+        ${result.password}
+        music
+        </h3>`)
+        $('.passwords').append(element)
+
+      })
+
+  })
+
+
+});
+
--- routes/passwords-api.js
+++ routes/passwords-api.js
@@ -7,7 +7,10 @@
 
 const express = require('express');
 const router = express.Router();
+const db = require('../db/connection');
 const userQueries = require('../db/queries/passwords');
+const generateRandomString = require('../routes/helper-functions')
+
 
 
 //GET passwords
@@ -28,12 +31,20 @@ router.get('/', (req, res) => { // the /passwords is assumed, just like listed i
 //GET password/:id
 router.get('/:id', (req, res) => {
   const id = req.params.id;
-  client.query('SELECT * FROM passwords  WHERE id = $1', [id])
+  db.query('SELECT * FROM passwords  WHERE id = $1', [id])
     .then((response) => {
       res.json(response.rows[0]);
     });
 });
 
-
+router.post('/', (req, res) => {
+db.query(`
+INSERT INTO passwords (user_id, username, url, email, password, category)
+VALUES ($1, $2, $3, $4, $5, $6) returning *;
+`, [req.session.userID, req.body.username, req.body.url, req.body.email, req.body.password, 'music'])
+  .then(resp=>{
+    res.json(resp.rows[0])
+  })
+})
 
 module.exports = router;
