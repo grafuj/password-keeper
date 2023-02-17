1845eab2c09b4f4635631fe5b4bb988dd745a7e3
Samson Poon <samsonkfpoon@gmail.com>
2023-02-17 17:09:14 +0000

    styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,33 +2,42 @@
 // Client facing scripts here
 $(() => {
 
-  $('#pw-copy').on('click', function() {
-    const myPassword = $('#pw-entry').val();
+  // Displays value of range slider in password generator
+  $('#pw-lengths').on('input', function () {
+    $(this).next('#range-val').html(this.value);
+  });
+  // Copies password from generate password box
+  $('#copy-genpass').on('click', function () {
+    const myPassword = $('#genpass').val();
     navigator.clipboard.writeText(myPassword);
     alert("Copied password successfully");
   });
 
-  $('#pw-db-copy').on('click', function() {
+  $('#pw-db-copy').on('click', function () {
     console.log('test1');
     const myPassword = $('#db-retrieved-pw').val();
     console.log('grabbed:', myPassword);
     navigator.clipboard.writeText(myPassword);
-    alert("Copied password successfully");
+    alert("Copied password successfully from db");
   });
 
+  // Delete a password
+  $("#pass-table").on('click', '.delete-btn', function () {
+    if(confirm("Are you sure you want to delete this password?"))
+    $(this).closest('tr').remove();
+  });
 
-
-  $('#generatebutton').on('click', function() {
-    const myLength = $('#pw-length').val();
-    const lowerCase = $('#pw-lowercase').is(':checked');
-    const upperCase = $('#pw-uppercase').is(':checked');
-    const number = $('#pw-number').is(':checked');
-    const sym = $('#pw-symbol').is(':checked');
+  $('#gen-button').on('click', function () {
+    const myLength = $('#pw-lengths').val();
+    const lowerCase = $('#pw-lowercases').is(':checked');
+    const upperCase = $('#pw-uppercases').is(':checked');
+    const number = $('#pw-numbers').is(':checked');
+    const sym = $('#pw-symbols').is(':checked');
 
 
     const password = genPassword(myLength, lowerCase, upperCase, number, sym);
     //put the password into the pw entry field
-    $("#pw-entry").val(password);
+    $("#genpass").val(password);
   });
 
   //returns a random string of desired length, with booleans to determine what the password should contain
@@ -61,11 +70,11 @@ $(() => {
     return randStr;
   };
   //can't use arrow functions,we want to use .this which will refer to what called the function as opposed to the parent
-  $('form#create-password').on('submit', function(event) {
+  $('form#create-password').on('submit', function (event) {
     event.preventDefault();
     const data = $(this).serialize();
     $.post('/api/passwords', data)
-      .then(function(result) {
+      .then(function (result) {
         const element = $(`
         <td>${result.username}</td>
         <td>${result.url}</td>
@@ -73,11 +82,17 @@ $(() => {
         <td>${result.password}</td>
         <td>${result.category}</td>
         <td><button id="pw-edit">Edit</button></td>
-        <td><button type = "submit" id ='pw-db-copy'>Copy</button></td>
+        <td><button type ="submit" id=pw-db-copy'>Copy</button></td>
         <td><button>Delete</button></td>
         `);
         $('.newestPw').append(element);
       });
+      // Reset text fields after submit
+      $("#url-entry").val("");
+      $("#username-entry").val("");
+      $("#email-entry").val("");
+      $("#pw-entry").val("");
+      $("#pw-category").val("");
   });
 });
 
